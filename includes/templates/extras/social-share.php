8637eef8f0e68d92dc915ca96bd604cc7a9ad6b5
Mohmed Elwany <mohmedelwany@yahoo.com>
2018-03-08 01:23:30 +0200

    add functions to all social-share buttons

--- includes/templates/extras/social-share.php
+++ includes/templates/extras/social-share.php
@@ -2,7 +2,7 @@
 <div class="cs-social-share clearfix">
   
   <div class="cs-share-this-product facebook">
-      <a class="fb-share" title="Share on Facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo the_permalink(); ?>">
+      <a class="fb-share" title="Share on Facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>">
       <div class="social-share-icon-block">
         <img src="https://theofficeplace.com/wp-content/uploads/share-icons/share-facebook.png">
         <span class="share-text">Share</span>
@@ -11,7 +11,7 @@
   </div>
   
   <div class="cs-share-this-product twitter ">
-    <a title="Share on Twitter" target="_blank" href="#">
+      <a title="Share on Twitter" target="_blank" href="http://twitter.com/share?text=<?php echo the_title(); ?>&url=<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>&hashtags=hashtag1,hashtag2,hashtag3">
       <div class="social-share-icon-block">
         <img src="https://theofficeplace.com/wp-content/uploads/share-icons/share-twitter.png">
         <span class="share-text">Tweet</span>
@@ -20,7 +20,7 @@
   </div>
   
   <div class="cs-share-this-product google-plus ">
-    <a title="Share on Google+" target="_blank" href="#">
+      <a title="Share on Google+" target="_blank" href="https://plus.google.com/share?url=<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>">
       <div class="social-share-icon-block">
         <img src="https://theofficeplace.com/wp-content/uploads/share-icons/share-google-plus.png">
         <span class="share-text">Share</span>
