c35372f54d277d8c130bfaf474341484e7e854ea
imtiazrayhan <irayhan.asif@gmail.com>
2018-05-02 00:36:17 -0400

    Minor changes to Vote buttons

--- includes/templates/extras/social-share.php
+++ includes/templates/extras/social-share.php
@@ -1,7 +1,6 @@
 <div class="clearfix"></div>
     <div class="share-buttons-container">
         <div class="col-md-12 row">
-            <span class='share-w'>share </span> 
             <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" target="_blank" class="btn-social btn-xs btn-facebook"><i class="fab fa-facebook-f"></i></a>
             <a href="https://plus.google.com/share?url=<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" target="_blank" class="btn-social btn-xs btn-google-plus"><i class="fab fa-google-plus-g"></i></a>
             <a href="http://twitter.com/share?text=<?php echo the_title(); ?> Coupon&url=<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" target="_blank" class="btn-social btn-xs btn-twitter"><i class="fab fa-twitter"></i></a>
--- includes/templates/extras/vote-system.php
+++ includes/templates/extras/vote-system.php
@@ -1,15 +1,21 @@
 <?php
     global $coupon_id;
-    $up_votes = array_filter(explode(",",get_post_meta($coupon_id,"_up",true)));
-    $down_votes = array_filter(explode(",",get_post_meta($coupon_id,"_down",true)));
-    $all_votes = array_merge($up_votes,$down_votes);
-    if(!empty($all_votes))
-        $percentage = count($up_votes) / count($all_votes) * 100;
+    $up_votes = array_filter( explode( ",", get_post_meta( $coupon_id, "_up", true ) ) );
+    $down_votes = array_filter( explode( ",", get_post_meta( $coupon_id, "_down", true ) ) );
+    $all_votes = array_merge( $up_votes,$down_votes );
+    if( !empty( $all_votes ) )
+        $percentage = ceil( count( $up_votes ) / count( $all_votes ) * 100 );
     else
         $percentage = 100;
 ?>
 <div class="vote-wrapper">
-    <a class="vote-up" href="#" data-id = "<?php echo $coupon_id; ?>"><span class="wpcd-tooltip">It works.</span><i class="fas fa-thumbs-up"></i></a>
+    <a class="vote-up" href="#" data-id = "<?php echo $coupon_id; ?>">
+        <span class="wpcd-tooltip"><?php echo __( 'It works!', 'wpcd-coupon' ); ?></span>
+        <i class="fas fa-thumbs-up"></i>
+    </a>
     <span class="vote-percent" data-id="<?php echo $coupon_id ?>"><?php echo $percentage; ?>% Success</span>
-    <a class="vote-down" href="#" data-id = "<?php echo $coupon_id; ?>"><span class="wpcd-tooltip">It Doesn't.</span><i class="fas fa-thumbs-down"></i></a>
+    <a class="vote-down" href="#" data-id = "<?php echo $coupon_id; ?>">
+        <span class="wpcd-tooltip"><?php echo __( 'It doesn\'t!', 'wpcd-coupon'); ?></span>
+        <i class="fas fa-thumbs-down"></i>
+    </a>
 </div>
