a7b4b0e432d7745e7b0e9dc8edf559972598f7de
imtiazrayhan <irayhan.asif@gmail.com>
2018-05-03 15:37:34 -0400

    Some prefix added

--- includes/templates/extras/vote-system.php
+++ includes/templates/extras/vote-system.php
@@ -8,13 +8,13 @@
     else
         $percentage = 100;
 ?>
-<div class="vote-wrapper">
-    <a class="vote-up" href="#" data-id = "<?php echo $coupon_id; ?>">
+<div class="wpcd-vote-wrapper">
+    <a class="wpcd-vote-up" href="#" data-id = "<?php echo $coupon_id; ?>">
         <span class="wpcd-tooltip"><?php echo __( 'It works!', 'wpcd-coupon' ); ?></span>
         <i class="fas fa-thumbs-up"></i>
     </a>
-    <span class="vote-percent" data-id="<?php echo $coupon_id ?>"><?php echo $percentage; ?>% Success</span>
-    <a class="vote-down" href="#" data-id = "<?php echo $coupon_id; ?>">
+    <span class="wpcd-vote-percent" data-id="<?php echo $coupon_id ?>"><?php echo $percentage; ?>% Success</span>
+    <a class="wpcd-vote-down" href="#" data-id = "<?php echo $coupon_id; ?>">
         <span class="wpcd-tooltip"><?php echo __( 'It doesn\'t!', 'wpcd-coupon'); ?></span>
         <i class="fas fa-thumbs-down"></i>
     </a>
