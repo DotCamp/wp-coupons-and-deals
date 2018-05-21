8bd7333e9c5d3671d9363c58229d43f4075a1803
imtiazrayhan <irayhan.asif@gmail.com>
2018-05-21 01:20:48 -0400

    Update: Some code cleaning

--- includes/classes/admin/wpcd-welcome-page.php
+++ includes/classes/admin/wpcd-welcome-page.php
@@ -22,56 +22,8 @@ class WPCD_Welcome_Page {
 	public static function init() {
 
 		add_action( 'admin_menu', array( __CLASS__, 'wpcd_welcome_menu_page' ) );
-		//add_action( 'admin_init', array( __CLASS__, 'wpcd_safe_welcome_redirect' ) );
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'wpcd_styles' ) );
 		add_action( 'admin_head', array( __CLASS__, 'remove_menu_entry' ) );
-
-	}
-
-	/**
-	 * Setting activation transient.
-	 *
-	 * @since 2.0
-	 */
-	public static function wpcd_welcome_activate() {
-
-		set_transient( '_wpcd_redirect_welcome', true, 30 );
-
-	}
-
-	/**
-	 * Setting deactivation transient.
-	 *
-	 * @since 2.0
-	 */
-	public static function wpcd_welcome_deactivate() {
-
-		delete_transient( '_wpcd_redirect_welcome' );
-
-	}
-
-	/**
-	 * Setting redirect.
-	 *
-	 * @since 2.0
-	 */
-	public static function wpcd_safe_welcome_redirect() {
-
-		// Bail if no activation redirect transient is present.
-		//if ( ! get_transient( '_wpcd_redirect_welcome' ) ) {
-		//	return;
-		//}
-
-		// Delete the redirect transient.
-		//delete_transient( '_wpcd_redirect_welcome' );
-
-		// Bail if activating from network or bulk sites.
-		//if ( is_network_admin() || isset( $_GET['activate-multi'] ) ) {
-		//	return;
-		//}
-
-		// Redirect to Welcome Page.
-		//wp_safe_redirect( add_query_arg( array( 'page' => 'wpcd_welcome_menu_page' ), admin_url( 'index.php' ) ) );
 
 	}
 
@@ -85,15 +37,15 @@ class WPCD_Welcome_Page {
 		global $wpcd_sub_menu;
 
 		$wpcd_sub_menu = add_submenu_page(
-			'index.php', // The slug name for the parent menu (or the file name of a standard WordPress admin page).
-			__( 'WP Coupons and Deals', 'wpcd-coupon' ), // The text to be displayed in the title tags of the page when the menu is selected.
-			__( 'WP Coupons and Deals', 'wpcd-coupon' ), // The text to be used for the menu.
-			'read', // The capability required for this menu to be displayed to the user.
-			'wpcd_welcome_menu_page', // The slug name to refer to this menu by (should be unique for this menu).
+			'index.php',
+			__( 'WP Coupons and Deals', 'wpcd-coupon' ),
+			__( 'WP Coupons and Deals', 'wpcd-coupon' ),
+			'read',
+			'wpcd_welcome_menu_page',
 			array(
 				__CLASS__,
 				'wpcd_welcome_page_content'
-			) // The function to be called to output the content for this page.
+			)
 		);
 
 	}
@@ -133,7 +85,6 @@ class WPCD_Welcome_Page {
 			'all'
 		);
 
-		wp_enqueue_style( 'wpcd-admin-style', WPCD_Plugin::instance()->plugin_assets . 'admin/css/admin.css', false );
 	}
 
 	/**
