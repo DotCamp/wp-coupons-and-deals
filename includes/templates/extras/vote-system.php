3c0ef430ab391025e6a3e9b03fbd8d71ddb75669
imtiazrayhan <irayhan.asif@gmail.com>
2018-06-05 15:47:21 -0400

    Fix: Voting Buttons

--- includes/templates/extras/vote-system.php
+++ includes/templates/extras/vote-system.php
@@ -3,7 +3,7 @@ global $coupon_id;
 $coupon_vote = get_option( 'wpcd_coupon-vote-system' );
 
 //Coupon vote
-if($coupon_vote == "on"){
+if ( $coupon_vote == "on" ){
     $up_votes = array_filter( explode( ",", get_post_meta( $coupon_id, "_up", true ) ) );
     $down_votes = array_filter( explode( ",", get_post_meta( $coupon_id, "_down", true ) ) );
     $all_votes = array_merge( $up_votes,$down_votes );
@@ -13,13 +13,13 @@ if($coupon_vote == "on"){
         $percentage = 100;
     ?>
     <div class="wpcd-vote-wrapper">
-        <a class="wpcd-vote-up" href="#" data-id = "<?php echo $coupon_id; ?>"><span class="wpcd-tooltip">It works.</span><i class="wpcd-thumbs-up"><img class="wpcd-svg" src="<?php echo WPCD_Plugin::instance()->plugin_assets.'svg/thumbs-up.svg'; ?>"/></i></a>
-    <span class="wpcd-vote-percent" data-id="<?php echo $coupon_id ?>"><?php echo $percentage; ?>% Success</span>
+        <a class="wpcd-vote-up" href="#" data-id = "<?php echo $coupon_id; ?>"><span class="wpcd-tooltip"><?php echo __( 'It works.', 'wpcd-coupon' ); ?></span><div class="wpcd-thumbs-up"><img class="wpcd-svg" src="<?php echo WPCD_Plugin::instance()->plugin_assets.'svg/thumbs-up.svg'; ?>"/></div></a>
+    <span class="wpcd-vote-percent" data-id="<?php echo $coupon_id ?>"><?php echo $percentage; ?>% <?php echo __( 'Success', 'wpcd-coupon' ); ?></span>
     <a class="wpcd-vote-down" href="#" data-id = "<?php echo $coupon_id; ?>">
         <span class="wpcd-tooltip"><?php echo __( 'It doesn\'t!', 'wpcd-coupon'); ?></span>
-        <i class="wpcd-thumbs-down">
+        <div class="wpcd-thumbs-down">
             <img class="wpcd-svg" src="<?php echo WPCD_Plugin::instance()->plugin_assets.'svg/thumbs-down.svg'; ?>"/>
-        </i>
+        </div>
     </a>
     </div>
 <?php
