9e0b6426fabefaac7d45d272d1fff070938c20ed
imtiazrayhan <irayhan.asif@gmail.com>
2019-03-10 18:40:01 -0400

    ADD: Gulp tasks for minifying CSS

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
+
+gulp.task('css-minify', function () {
+    gulp.
+        src('./assets/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./assets/css/'))
+});
+
+gulp.task('admin-css-minify', function () {
+    gulp.
+        src('./assets/admin/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./assets/admin/css/'))
+});
