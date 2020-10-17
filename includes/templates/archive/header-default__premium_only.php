1b330edea6477a64e3a2de065d7f8694044e069a
Nikolay Shabarkov <nikolayshabarkov@yandex.ru>
2020-10-17 19:09:42 +0400

    Changed CSS for template six and some changes for search bar

--- includes/templates/archive/header-default__premium_only.php
+++ includes/templates/archive/header-default__premium_only.php
@@ -7,12 +7,12 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
         ?>
         <section class="wpcd_archive_section wpcd_clearfix">
         <?php
-            
+
             $archive_category_setting = get_option( 'wpcd_archive-munu-categories' );
             if( !($all_coupon_text = get_option( 'wpcd_all-coupon-text')) ){
                 $all_coupon_text = __( 'All Coupons', 'wpcd-coupon' );
             }
-            
+
             if( $archive_category_setting == 'vendor' ) {
                 $terms = get_terms( 'wpcd_coupon_vendor' );
                 $wpcd_term_field_name = 'wpcd_vendor';
@@ -24,7 +24,7 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
                 $wpcd_category_menu_title = 'Categories';
                 $wpcd_js_data_tax_name = 'data-category';
             }
-            
+
             if ( !empty( $terms ) && !is_wp_error( $terms ) && !$disable_menu ):
                 $current_url_final = wpcd_preparationMenuLinks( $wpcd_term_field_name );
                 $current_url_final_all = $current_url_final['all'];
@@ -33,7 +33,7 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
                 <div class="wpcd_div_nav_block">
                     <div class="wpcd_cats">
                         <ul id="wpcd_cat_ul" class="wpcd_dropdown wpcd_categories_in_dropdown">
-                            <?php if( !WPCD_Amp::wpcd_amp_is() ): ?>    
+                            <?php if( !WPCD_Amp::wpcd_amp_is() ): ?>
                                 <a href="javascript:void(0)" class="wpcd_dropbtn">
                                     <?php echo __( $wpcd_category_menu_title, 'wpcd-coupon' ); ?>
                                 </a>
@@ -44,7 +44,7 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
                             <?php endif; ?>
                             <div class="wpcd_dropdown-content">
                                 <li>
-                                    <?php 
+                                    <?php
                                         if( ! isset( $_GET[$wpcd_term_field_name] ) || $_GET[$wpcd_term_field_name] == 'all' || $_GET[$wpcd_term_field_name] == '' ) {
                                             $wpcd_dropdown_content = ' active';
                                         } else {
@@ -55,7 +55,7 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
                                         <?php echo $all_coupon_text; ?>
                                     </a>
                                 </li>
-                                <?php foreach ( $terms as $term ): 
+                                <?php foreach ( $terms as $term ):
                                     if( isset( $_GET[$wpcd_term_field_name] ) && $_GET[$wpcd_term_field_name] == $term->slug ) {
                                         $wpcd_dropdown_content = ' active';
                                     } else {
@@ -74,11 +74,11 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
                     </div>
                     <?php if( !WPCD_Amp::wpcd_amp_is() ): ?>
                         <div id="wpcd_searchbar">
-                            <ul id="wpcd_cat_ul_search">
-                                <li class="wpcd_searchbar_search">
+                            <div id="wpcd_cat_ul_search">
+                                <div class="wpcd_searchbar_search">
                                     <input type="text" placeholder="Search">
-                                </li>
-                            </ul>
+                                </div>
+                            </div>
                             <!--
                             <ul id="wpcd_cat_ul" class="wpcd_search2">
                                 <li class="wpcd_searchbar_search">
@@ -92,7 +92,7 @@ if ( $parent == 'header' || $parent == 'headerANDfooter' ):
                 </div>
                 <div class="wpcd_cat_ul_border"></div>
             <?php endif; ?>
-        <?php if( !WPCD_Amp::wpcd_amp_is() ): ?>    
+        <?php if( !WPCD_Amp::wpcd_amp_is() ): ?>
         <div class="wpcd_coupon_archive_container_main">
             <div class="wpcd_coupon_loader wpcd_coupon_hidden_loader">
                 <img src="<?php echo WPCD_Plugin::instance()->plugin_assets . 'img/loading.gif'; ?>">
