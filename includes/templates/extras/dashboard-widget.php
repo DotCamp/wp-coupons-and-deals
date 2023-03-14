0d5cf585c2c4f83997ea47df48b65c534b946841
Imtiaz Rayhan <irayhan.asif@gmail.com>
2023-03-14 00:03:21 -0400

    Dashboard widget fix

--- includes/templates/extras/dashboard-widget.php
+++ includes/templates/extras/dashboard-widget.php
@@ -9,17 +9,14 @@ $trash          = number_format_i18n( $num_posts->trash );
 $category_count = wp_count_terms( 'wpcd_coupon_category' );
 $vendor_count   = wp_count_terms( 'wpcd_coupon_vendor' );
 
-if ( current_user_can( 'edit_posts' ) ) {
-    $published      = "<a href=" . esc_url( admin_url( 'edit.php?post_status=publish&post_type=wpcd_coupons' ) ) . ">$published</a>";
-    $draft          = "<a href=" . esc_url( admin_url( 'edit.php?post_status=draft&post_type=wpcd_coupons' ) ) . ">$draft</a>";
-    $trash          = "<a href=" . esc_url( admin_url( 'edit.php?post_status=trash&post_type=wpcd_coupons' ) ) . ">$trash</a>";
-    $category_count = "<a href=" . esc_url( admin_url( 'edit-tags.php?taxonomy=wpcd_coupon_category&post_type=wpcd_coupons' ) ) . ">$category_count</a>";
-    $vendor_count   = "<a href=" . esc_url( admin_url( 'edit-tags.php?taxonomy=wpcd_coupon_vendor&post_type=wpcd_coupons' ) ) . ">$vendor_count</a>";
-}
-
 $install_ub_url = \wp_nonce_url(
 	\self_admin_url( 'update.php?action=install-plugin&plugin=ultimate-blocks' ),
 	'install-plugin_ultimate-blocks'
+);
+
+$install_wptb_url = \wp_nonce_url(
+	\self_admin_url( 'update.php?action=install-plugin&plugin=wp-table-builder' ),
+	'install-plugin_wp-table-builder'
 );
 
 ?>
@@ -78,7 +75,7 @@ $install_ub_url = \wp_nonce_url(
 <div style="margin: 0px -12px 0; padding: 12px 12px 0; border-top: 1px solid #eee;">
 	<p style="margin: 0">
 		<?php echo __( 'Using WP Coupons and Deals version ', 'wp-coupons-and-deals' ) . '<strong>' .WPCD_Plugin::PLUGIN_VERSION . '</strong>'; ?>
-		(<a href="https://wpcouponsdeals.com/changelog/" target="_blank">
+		(<a href="https://wordpress.org/plugins/wp-coupons-and-deals/#developers" target="_blank">
 			<?php echo __( 'Changelog', 'wp-coupons-and-deals'); ?>
 		</a>)
 	</p>
@@ -92,6 +89,11 @@ if ( current_user_can( "manage_options" ) ) {
 			<p style="margin: 0"><?php echo __( 'Recommended Plugin: ', 'wp-coupons-and-deals' );?><b><?php echo __( 'Ultimate Blocks', 'wp-coupons-and-deals' ); ?></b> -
 				<a href="<?php echo esc_url( $install_ub_url ); ?>"><?php echo __( 'Install', 'wp-coupons-and-deals' ); ?></a> |
 				<a href="https://ultimateblocks.com/?utm_source=wpdashboard&utm_medium=widget" target="_blank"><?php echo __( 'Learn More', 'wp-coupons-and-deals' ); ?></a></p>
+	</div>
+	<div style="margin: 12px -12px 0; padding: 12px 12px 0; border-top: 1px solid #eee;">
+			<p style="margin: 0"><?php echo __( 'Recommended Plugin: ', 'wp-coupons-and-deals' );?><b><?php echo __( 'WP Table Builder', 'wp-coupons-and-deals' ); ?></b> -
+				<a href="<?php echo esc_url( $install_ub_url ); ?>"><?php echo __( 'Install', 'wp-coupons-and-deals' ); ?></a> |
+				<a href="https://wptablebuilder.com/?utm_source=wpdashboard&utm_medium=widget" target="_blank"><?php echo __( 'Learn More', 'wp-coupons-and-deals' ); ?></a></p>
 	</div>
 	<?php
 	}
